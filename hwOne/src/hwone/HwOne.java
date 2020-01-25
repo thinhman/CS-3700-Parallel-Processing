b924ab7c6c2c5e81945ddb5670c9a13099e202a9
thinhman <thinhnguyen.nguyen2353@gmail.com>
2020-01-25 12:09:48 -0800

    Print Name

--- hwOne/src/hwone/HwOne.java
+++ hwOne/src/hwone/HwOne.java
@@ -3,7 +3,7 @@ package hwone;
 public class HwOne {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, my name is Thinh");
     }
     
 }
