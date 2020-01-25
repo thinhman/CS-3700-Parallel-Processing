a9543e1c738ef0bc8e2a0d3dcf2bbc62809ba2bb
thinhman <thinhnguyen.nguyen2353@gmail.com>
2020-01-25 12:13:07 -0800

    Print Goodbye message

--- hwOne/src/hwone/HwOne.java
+++ hwOne/src/hwone/HwOne.java
@@ -3,7 +3,7 @@ package hwone;
 public class HwOne {
 
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Thinh");
+        System.out.println("Goodbye, I finished my CS 3700 HW1");
     }
     
 }
