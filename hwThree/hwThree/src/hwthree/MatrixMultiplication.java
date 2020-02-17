1a98820ecd856c829595e8096b90b29432cc3bc0
thinhman <thinhnguyen.nguyen2353@gmail.com>
2020-02-17 13:16:58 -0800

    HW3

--- hwThree/hwThree/src/hwthree/MatrixMultiplication.java
+++ hwThree/hwThree/src/hwthree/MatrixMultiplication.java
@@ -1,5 +1,7 @@
 package hwthree;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,24 +11,26 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class MatrixMultiplication {
     
-    
     public static void main(String [] args) throws InterruptedException{
-        final int m = 100;
-        final int n = 50;
-        final int p = 65;
+        final int m = 1000;
+        final int n = 1000;
+        final int p = 1000;
         
         int[][] a = new int[m][n];
         int[][] b = new int[n][p];
+        int[][] c = new int[m][p];
         
+        
+        System.out.println("m = " + m + " n = " + n + " p = " + p);
         //initalize Matrix A and print
         System.out.println("Matrix A");
         for(int row = 0; row < m; row++){
             System.out.print("|   ");
             for(int col = 0; col < n; col++){
-                a[row][col] = ThreadLocalRandom.current().nextInt(2)+1;
+                a[row][col] = ThreadLocalRandom.current().nextInt(10)+1;
                 System.out.print(a[row][col] + "   ");
             }
-            System.out.println("|");
+           System.out.println("|");
         }
         
         //initalize Matrix A and print
@@ -34,35 +38,29 @@ public class MatrixMultiplication {
         for(int row = 0; row < n; row++){
             System.out.print("|   ");
             for(int col = 0; col < p; col++){
-                b[row][col] = ThreadLocalRandom.current().nextInt(2)+1;
+                b[row][col] = ThreadLocalRandom.current().nextInt(10)+1;
                 System.out.print(b[row][col] + "   ");
             }
             System.out.println("|");
         }
         
-        //Record Time of Execution
-        System.out.println("Matrix Multiplication start...");
-	long start = System.currentTimeMillis();
-        
-        MatrixMulti mm = new MatrixMulti(a, b, m, p);
+        MatrixMulti mm = new MatrixMulti(a, b, c, m, n, p);
         mm.multiply();
-        
-        long finish = System.currentTimeMillis();
 	
         //Print Matrix C and check
-        System.out.println("Matrix B");
-        int[][] res = mm.getResult();
+        System.out.println("Matrix C");
         for(int row = 0; row < m; row++){
             System.out.print("|   ");
             for(int col = 0; col < p; col++){
-                System.out.print(res[row][col] + "   ");
+                System.out.print(c[row][col] + "   ");
             }
             System.out.println("|");
         }
         
-        System.out.format("Time elapsed: %d ms\n", finish-start);
     }   
 }
+
+
 class MatrixMultiThread implements Runnable{
     MatrixMulti Matrix;
     int m;
@@ -112,69 +110,87 @@ class MatrixMulti {
     int[][] A;
     int[][] B;
     int[][] C;
+    int m;
+    int n;
+    int p;
+    
     //Vary # of Threads [1, 2, 4, 8]
-    int numThreads = 8;
-    ExecutorService exec = Executors.newFixedThreadPool(numThreads);
+    int numProcessors = Runtime.getRuntime().availableProcessors();
+    final int[] numThreads = {1, 2, 4, 8};
     int work;
     
-    public MatrixMulti(int[][] a, int[][] b, int m, int p){
+    public MatrixMulti(int[][] a, int[][] b, int[][] c, int m, int n, int p){
         this.A = a;
         this.B = b;
-        this.C = new int[m][p];
+        this.C = c;
+        this.m = m;
+        this.n = n;
+        this.p = p;
     }
     
     public void multiply() throws InterruptedException {
-        Future[] fut = new Future[numThreads];
-        //Split Thread workload by max(C.row.length, C.col.length)
-        if(A.length >= B[0].length)
-        {
-            
-            int split = A.length/numThreads;
-            int start = 0;
-            int end = start + split;
-            work = A.length - end;
-            
-            for(int x = 0; x < numThreads; x++){
-                
-                Future f;
-                if(!(x+1 < numThreads)){
-                    f = exec.submit(new MatrixMultiThread(this, start, work, true));
-                }else{
-                    f = exec.submit(new MatrixMultiThread(this, start, end, true));
+        
+        //Record Time of Execution
+        System.out.println("Matrix Multiplication start...");
+        for(int t = 0; t < numThreads.length; t++){
+            System.out.println("Number of Avaliable Processors: " + numProcessors);
+            System.out.println("Number of Threads used: " + numThreads[t]);
+            Instant startTime = Instant.now();
+            ExecutorService exec = Executors.newFixedThreadPool(numThreads[t]);
+            Future[] fut = new Future[numThreads[t]];
+
+            //Split Thread workload by max(C.row.length, C.col.length)
+            if(A.length >= B[0].length)
+            {
+
+                int split = A.length/numThreads[t];
+                int start = 0;
+                int end = start + split;
+                work = A.length - end;
+
+                for(int x = 0; x < numThreads[t]; x++){
+
+                    Future f;
+                    if(!(x+1 < numThreads[t]) && numThreads[t] != 1){
+                        f = exec.submit(new MatrixMultiThread(this, start, work, true));
+                    }else{
+                        f = exec.submit(new MatrixMultiThread(this, start, end, true));
+                    }
+                    start += split;
+                    end += split;
+                    fut[x] = f;
+                }   
+            }else{
+                int split = C[0].length/numThreads[t];
+                int start = 0;
+                int end = start + split;
+                work = C[0].length - end;
+                for(int x = 0; x < numThreads[t]; x++){
+                    Future f;
+                    if(!(x+1 < numThreads[t])&& numThreads[t] != 1){
+                        f = exec.submit(new MatrixMultiThread(this, start, work, false));
+                    }else{
+                        f = exec.submit(new MatrixMultiThread(this, start, end, false));
+                    }
+                    start += split;
+                    end += split;
+                    fut[x] = f;
                 }
-                start += split;
-                end += split;
-                fut[x] = f;
             }
-            //Future f = exec.submit(new WorkerThread(A, B, C, 0, 0, 0, 0, 0, 0, A.length, exec, numThreads));
-            
-            
-        }else{
-            int split = C[0].length/numThreads;
-            int start = 0;
-            int end = start + split;
-            work = C[0].length - end;
-            for(int x = 0; x < numThreads; x++){
-                Future f;
-                if(!(x+1 < numThreads)){
-                    f = exec.submit(new MatrixMultiThread(this, start, work, false));
-                }else{
-                    f = exec.submit(new MatrixMultiThread(this, start, end, false));
+            for(Future f : fut){
+                try{
+                    f.get();
+                }catch(ExecutionException e){
+
                 }
-                start += split;
-                end += split;
-                fut[x] = f;
             }
+
+            exec.shutdown();
+            Instant finish = Instant.now();
+            long timeElapsed = Duration.between(startTime, finish).toNanos();
+            System.out.format("Time elapsed: %d ns\n", timeElapsed);    
         }
-        for(Future f : fut){
-            try{
-                f.get();
-            }catch(ExecutionException e){
-                
-            }
-        }
-        exec.shutdown();
-	
+        
     }
     
     public int[][] getResult() {
